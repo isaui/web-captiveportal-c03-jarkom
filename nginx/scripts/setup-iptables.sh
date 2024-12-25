293d6b0d63d201d3ba2d95df53cf2388640b16f9
isaui <isadestroyed17@gmail.com>
2024-12-25 18:41:50 +0700

    alhamdulillah

--- nginx/scripts/setup-iptables.sh
+++ nginx/scripts/setup-iptables.sh
@@ -10,11 +10,18 @@ iptables -t nat -F
 # Allow established connections
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# NAT for internet access
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# NAT for internet access - gunakan ens4 sebagai interface utama
+iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
 
 # Redirect HTTP traffic ke port 80
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 80
 
-# Redirect HTTPS traffic ke port 443 (optional)
+# Redirect HTTPS traffic ke port 443
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 443
+
+# Tambahan untuk forwarding ke docker network
+iptables -A FORWARD -i ens4 -o br-$(docker network ls | grep portal_network | awk '{print $1}') -j ACCEPT
+iptables -A FORWARD -o ens4 -i br-$(docker network ls | grep portal_network | awk '{print $1}') -j ACCEPT
+
+# Simpan aturan
+netfilter-persistent save
